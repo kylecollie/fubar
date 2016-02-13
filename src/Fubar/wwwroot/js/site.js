494ca1e4669bd462bd9b8337f9a60ec043ced6f1
Kyle <kcollie@hotmail.com>
2016-02-13 11:21:10 -0500

    Finished JS

---
+++ src/Fubar/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("The Lumberjack");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background", "#888");
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.css("background", "");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+})();
