04b6744bdd2adf8f68ee2fceec48206bbfa2e9c1
Kyle <kcollie@hotmail.com>
2016-02-14 00:13:02 -0500

    Finished MVC6 module

---
+++ src/Fubar/Services/MailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fubar.Services
+{
+    public class MailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string body)
+        {
+            Debug.WriteLine($"Sending mail: To: {to}, Subject {subject}");
+            return true;
+        }
+    }
+}
